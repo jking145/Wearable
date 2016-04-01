b0915b70070d642accfe19524a9d81f029b0355f
jking145 <julieking39@hotmail.com>
2016-04-01 13:45:33 -0400

    Button Code - test
    test of button() which activates calibration()

--- Wearable - Arduino/Wearable/Wearable.ino
+++ Wearable - Arduino/Wearable/Wearable.ino
@@ -1,40 +1,40 @@
 
 /*
 >> Pulse Sensor Amped 1.1 <<
-This code is for Pulse Sensor Amped by Joel Murphy and Yury Gitman
-    www.pulsesensor.com 
-    >>> Pulse Sensor purple wire goes to Analog Pin 0 <<<
-Pulse Sensor sample aquisition and processing happens in the background via Timer 2 interrupt. 2mS sample rate.
-PWM on pins 3 and 11 will not work when using this code, because we are using Timer 2!
-The following variables are automatically updated:
-Signal :    int that holds the analog signal data straight from the sensor. updated every 2mS.
-IBI  :      int that holds the time interval between beats. 2mS resolution.
-BPM  :      int that holds the heart rate value, derived every beat, from averaging previous 10 IBI values.
-QS  :       boolean that is made true whenever Pulse is found and BPM is updated. User must reset.
-Pulse :     boolean that is true when a heartbeat is sensed then false in time with pin13 LED going out.
-
-This code is designed with output serial data to Processing sketch "PulseSensorAmped_Processing-xx"
-The Processing sketch is a simple data visualizer. 
-All the work to find the heartbeat and determine the heartrate happens in the code below.
-Pin 13 LED will blink with heartbeat.
-If you want to use pin 13 for something else, adjust the interrupt handler
-It will also fade an LED on pin fadePin with every beat. Put an LED and series resistor from fadePin to GND.
-Check here for detailed code walkthrough:
-http://pulsesensor.myshopify.com/pages/pulse-sensor-amped-arduino-v1dot1
-
-Code Version 02 by Joel Murphy & Yury Gitman  Fall 2012
-This update changes the HRV variable name to IBI, which stands for Inter-Beat Interval, for clarity.
-Switched the interrupt to Timer2.  500Hz sample rate, 2mS resolution IBI value.
-Fade LED pin moved to pin 5 (use of Timer2 disables PWM on pins 3 & 11).
-Tidied up inefficiencies since the last version. 
-*/
+ This code is for Pulse Sensor Amped by Joel Murphy and Yury Gitman
+ www.pulsesensor.com 
+ >>> Pulse Sensor purple wire goes to Analog Pin 0 <<<
+ Pulse Sensor sample aquisition and processing happens in the background via Timer 2 interrupt. 2mS sample rate.
+ PWM on pins 3 and 11 will not work when using this code, because we are using Timer 2!
+ The following variables are automatically updated:
+ Signal :    int that holds the analog signal data straight from the sensor. updated every 2mS.
+ IBI  :      int that holds the time interval between beats. 2mS resolution.
+ BPM  :      int that holds the heart rate value, derived every beat, from averaging previous 10 IBI values.
+ QS  :       boolean that is made true whenever Pulse is found and BPM is updated. User must reset.
+ Pulse :     boolean that is true when a heartbeat is sensed then false in time with pin13 LED going out.
+ 
+ This code is designed with output serial data to Processing sketch "PulseSensorAmped_Processing-xx"
+ The Processing sketch is a simple data visualizer. 
+ All the work to find the heartbeat and determine the heartrate happens in the code below.
+ Pin 13 LED will blink with heartbeat.
+ If you want to use pin 13 for something else, adjust the interrupt handler
+ It will also fade an LED on pin fadePin with every beat. Put an LED and series resistor from fadePin to GND.
+ Check here for detailed code walkthrough:
+ http://pulsesensor.myshopify.com/pages/pulse-sensor-amped-arduino-v1dot1
+ 
+ Code Version 02 by Joel Murphy & Yury Gitman  Fall 2012
+ This update changes the HRV variable name to IBI, which stands for Inter-Beat Interval, for clarity.
+ Switched the interrupt to Timer2.  500Hz sample rate, 2mS resolution IBI value.
+ Fade LED pin moved to pin 5 (use of Timer2 disables PWM on pins 3 & 11).
+ Tidied up inefficiencies since the last version. 
+ */
 
 
 //  VARIABLES
-int pulsePin = 0;                 // Pulse Sensor purple wire connected to analog pin 0
-int blinkPin = 13;                // pin to blink led at each beat
-int fadePin = 5;                  // pin to do fancy classy fading blink at each beat
-int fadeRate = 0;                 // used to fade LED on with PWM on fadePin
+//int pulsePin = 0;                 // Pulse Sensor purple wire connected to analog pin 0
+//int blinkPin = 13;                // pin to blink led at each beat
+//int fadePin = 5;                  // pin to do fancy classy fading blink at each beat
+//int fadeRate = 0;                 // used to fade LED on with PWM on fadePin
 
 volatile int rate[10];                    // used to hold last ten IBI values
 volatile unsigned long sampleCounter = 0;          // used to determine pulse timing
@@ -47,11 +47,16 @@ volatile boolean firstBeat = true;        // used to seed rate array so we start
 volatile boolean secondBeat = true;       // used to seed rate array so we startup with reasonable BPM
 
 
-//*---------- CUSTOM VAR ----------*
- boolean startup = false;
- int counter = 0;
- unsigned long timer = 0;
- 
+//*---------- CUSTOM VAR CALIBRATION---------*
+boolean startup = false;
+int counter = 0;
+unsigned long timer = 0;
+
+//*---------- CUSTOM VAR BUTTON---------*
+int buttonPin = 2;
+int ledPin = 4;
+int buttonState = 0;
+
 
 // these variables are volatile because they are used during the interrupt service routine!
 volatile int BPM;                   // used to hold the pulse rate
@@ -66,65 +71,84 @@ void setup(){
   pinMode(fadePin,OUTPUT);          // pin that will fade to your heartbeat!
   Serial.begin(115200);             // we agree to talk fast!
   interruptSetup();                 // sets up to read Pulse Sensor signal every 2mS 
-   // UN-COMMENT THE NEXT LINE IF YOU ARE POWERING The Pulse Sensor AT LOW VOLTAGE, 
-   // AND APPLY THAT VOLTAGE TO THE A-REF PIN
-   //analogReference(EXTERNAL);   
+  // UN-COMMENT THE NEXT LINE IF YOU ARE POWERING The Pulse Sensor AT LOW VOLTAGE, 
+  // AND APPLY THAT VOLTAGE TO THE A-REF PIN
+  //analogReference(EXTERNAL);   
 }
 
 
 void loop(){
-//  sendDataToProcessing('S', Signal);     // send Processing the raw Pulse Sensor data
-//  if (QS == true){                       // Quantified Self flag is true when arduino finds a heartbeat
-//        fadeRate = 255;                  // Set 'fadeRate' Variable to 255 to fade LED with pulse
-//        sendDataToProcessing('B',BPM);   // send heart rate with a 'B' prefix
-//        sendDataToProcessing('Q',IBI);   // send time between beats with a 'Q' prefix
-//        QS = false;                      // reset the Quantified Self flag for next time    
-//     }
-  
+  //  sendDataToProcessing('S', Signal);     // send Processing the raw Pulse Sensor data
+  //  if (QS == true){                       // Quantified Self flag is true when arduino finds a heartbeat
+  //        fadeRate = 255;                  // Set 'fadeRate' Variable to 255 to fade LED with pulse
+  //        sendDataToProcessing('B',BPM);   // send heart rate with a 'B' prefix
+  //        sendDataToProcessing('Q',IBI);   // send time between beats with a 'Q' prefix
+  //        QS = false;                      // reset the Quantified Self flag for next time    
+  //     }
+
   //ledFadeToBeat();
-  
+
   //delay(20);                             //  take a break
-  
-  calibrate();
-  
+
+  // read the state of the pushbutton value:
+  button();
+ 
 }
 
 //code here
-  
+
+void button() {
+ buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH) {     
+    // run calibration 
+    delay(2000);
+    calibrate();
+    digitalWrite(ledPin, HIGH);
+  } 
+  else {
+    digitalWrite(ledPin, LOW);    
+  } 
+}
+
+
 void calibrate() {
-    
-    if(!startup) {
+
+  if(!startup) {
     // FIRST RUN
-     timer = millis();
-     int average = 0;
-     
-   while(millis() - timer < 15000) {
-     // stay in here
-     // IBI varialble 
-     if (QS == true) {
-     Serial.print("Counter: "); Serial.print(counter++);
-     Serial.print(" BPM: "); Serial.print(BPM);
-     average += BPM;
-     Serial.println("");
-       QS = false;
-     }
+    timer = millis();
+    int average = 0;
+
+    while(millis() - timer < 15000) {
+      // stay in here
+      // IBI varialble 
+      if (QS == true) {
+        Serial.print("Counter: "); 
+        Serial.print(counter++);
+        Serial.print(" BPM: "); 
+        Serial.print(BPM);
+        average += BPM;
+        Serial.println("");
+        QS = false;
+      }
+    }
+
+    average = average / counter;
+    Serial.print(" AVERAGE: "); 
+    Serial.print(average);
+    Serial.println("");
+    startup = true;
+    counter = 0;
   }
-  
-  average = average / counter;
-  Serial.print(" AVERAGE: "); Serial.print(average);
-  Serial.println("");
-  startup = true;
-  counter = 0;
-  }
-    
-    /// other 
+
+  /// other 
 }
 
 void ledFadeToBeat(){
-    fadeRate -= 15;                         //  set LED fade value
-    fadeRate = constrain(fadeRate,0,255);   //  keep LED fade value from going into negative numbers!
-    analogWrite(fadePin,fadeRate);          //  fade LED
-  }
+  fadeRate -= 15;                         //  set LED fade value
+  fadeRate = constrain(fadeRate,0,255);   //  keep LED fade value from going into negative numbers!
+  analogWrite(fadePin,fadeRate);          //  fade LED
+}
 
 
 //void sendDataToProcessing(char symbol, int data ){
@@ -145,80 +169,81 @@ void interruptSetup(){
 // THIS IS THE TIMER 2 INTERRUPT SERVICE ROUTINE. 
 // Timer 2 makes sure that we take a reading every 2 miliseconds
 ISR(TIMER2_COMPA_vect){                         // triggered when Timer2 counts to 124
-    cli();                                      // disable interrupts while we do this
-    Signal = analogRead(pulsePin);              // read the Pulse Sensor 
-    sampleCounter += 2;                         // keep track of the time in mS with this variable
-    int N = sampleCounter - lastBeatTime;       // monitor the time since the last beat to avoid noise
+  cli();                                      // disable interrupts while we do this
+  Signal = analogRead(pulsePin);              // read the Pulse Sensor 
+  sampleCounter += 2;                         // keep track of the time in mS with this variable
+  int N = sampleCounter - lastBeatTime;       // monitor the time since the last beat to avoid noise
 
-//  find the peak and trough of the pulse wave
-    if(Signal < thresh && N > (IBI/5)*3){       // avoid dichrotic noise by waiting 3/5 of last IBI
-        if (Signal < T){                        // T is the trough
-            T = Signal;                         // keep track of lowest point in pulse wave 
-         }
-       }
-      
-    if(Signal > thresh && Signal > P){          // thresh condition helps avoid noise
-        P = Signal;                             // P is the peak
-       }                                        // keep track of highest point in pulse wave
-    
+    //  find the peak and trough of the pulse wave
+  if(Signal < thresh && N > (IBI/5)*3){       // avoid dichrotic noise by waiting 3/5 of last IBI
+    if (Signal < T){                        // T is the trough
+      T = Signal;                         // keep track of lowest point in pulse wave 
+    }
+  }
+
+  if(Signal > thresh && Signal > P){          // thresh condition helps avoid noise
+    P = Signal;                             // P is the peak
+  }                                        // keep track of highest point in pulse wave
+
   //  NOW IT'S TIME TO LOOK FOR THE HEART BEAT
   // signal surges up in value every time there is a pulse
-if (N > 250){                                   // avoid high frequency noise
-  if ( (Signal > thresh) && (Pulse == false) && (N > (IBI/5)*3) ){        
-    Pulse = true;                               // set the Pulse flag when we think there is a pulse
-    digitalWrite(blinkPin,HIGH);                // turn on pin 13 LED
-    IBI = sampleCounter - lastBeatTime;         // measure time between beats in mS
-    lastBeatTime = sampleCounter;               // keep track of time for next pulse
-         
-         if(firstBeat){                         // if it's the first time we found a beat, if firstBeat == TRUE
-             firstBeat = false;                 // clear firstBeat flag
-             return;                            // IBI value is unreliable so discard it
-            }   
-         if(secondBeat){                        // if this is the second beat, if secondBeat == TRUE
-            secondBeat = false;                 // clear secondBeat flag
-               for(int i=0; i<=9; i++){         // seed the running total to get a realisitic BPM at startup
-                    rate[i] = IBI;                      
-                    }
-            }
-          
-    // keep a running total of the last 10 IBI values
-    word runningTotal = 0;                   // clear the runningTotal variable    
+  if (N > 250){                                   // avoid high frequency noise
+    if ( (Signal > thresh) && (Pulse == false) && (N > (IBI/5)*3) ){        
+      Pulse = true;                               // set the Pulse flag when we think there is a pulse
+      digitalWrite(blinkPin,HIGH);                // turn on pin 13 LED
+      IBI = sampleCounter - lastBeatTime;         // measure time between beats in mS
+      lastBeatTime = sampleCounter;               // keep track of time for next pulse
 
-    for(int i=0; i<=8; i++){                // shift data in the rate array
-          rate[i] = rate[i+1];              // and drop the oldest IBI value 
-          runningTotal += rate[i];          // add up the 9 oldest IBI values
+      if(firstBeat){                         // if it's the first time we found a beat, if firstBeat == TRUE
+        firstBeat = false;                 // clear firstBeat flag
+        return;                            // IBI value is unreliable so discard it
+      }   
+      if(secondBeat){                        // if this is the second beat, if secondBeat == TRUE
+        secondBeat = false;                 // clear secondBeat flag
+        for(int i=0; i<=9; i++){         // seed the running total to get a realisitic BPM at startup
+          rate[i] = IBI;                      
         }
-        
-    rate[9] = IBI;                          // add the latest IBI to the rate array
-    runningTotal += rate[9];                // add the latest IBI to runningTotal
-    runningTotal /= 10;                     // average the last 10 IBI values 
-    BPM = 60000/runningTotal;               // how many beats can fit into a minute? that's BPM!
-    QS = true;                              // set Quantified Self flag 
-    // QS FLAG IS NOT CLEARED INSIDE THIS ISR
+      }
+
+      // keep a running total of the last 10 IBI values
+      word runningTotal = 0;                   // clear the runningTotal variable    
+
+      for(int i=0; i<=8; i++){                // shift data in the rate array
+        rate[i] = rate[i+1];              // and drop the oldest IBI value 
+        runningTotal += rate[i];          // add up the 9 oldest IBI values
+      }
+
+      rate[9] = IBI;                          // add the latest IBI to the rate array
+      runningTotal += rate[9];                // add the latest IBI to runningTotal
+      runningTotal /= 10;                     // average the last 10 IBI values 
+      BPM = 60000/runningTotal;               // how many beats can fit into a minute? that's BPM!
+      QS = true;                              // set Quantified Self flag 
+      // QS FLAG IS NOT CLEARED INSIDE THIS ISR
     }                       
-}
+  }
 
   if (Signal < thresh && Pulse == true){     // when the values are going down, the beat is over
-      digitalWrite(blinkPin,LOW);            // turn off pin 13 LED
-      Pulse = false;                         // reset the Pulse flag so we can do it again
-      amp = P - T;                           // get amplitude of the pulse wave
-      thresh = amp/2 + T;                    // set thresh at 50% of the amplitude
-      P = thresh;                            // reset these for next time
-      T = thresh;
-     }
-  
+    digitalWrite(blinkPin,LOW);            // turn off pin 13 LED
+    Pulse = false;                         // reset the Pulse flag so we can do it again
+    amp = P - T;                           // get amplitude of the pulse wave
+    thresh = amp/2 + T;                    // set thresh at 50% of the amplitude
+    P = thresh;                            // reset these for next time
+    T = thresh;
+  }
+
   if (N > 2500){                             // if 2.5 seconds go by without a beat
-      thresh = 512;                          // set thresh default
-      P = 512;                               // set P default
-      T = 512;                               // set T default
-      lastBeatTime = sampleCounter;          // bring the lastBeatTime up to date        
-      firstBeat = true;                      // set these to avoid noise
-      secondBeat = true;                     // when we get the heartbeat back
-     }
-  
-  
+    thresh = 512;                          // set thresh default
+    P = 512;                               // set P default
+    T = 512;                               // set T default
+    lastBeatTime = sampleCounter;          // bring the lastBeatTime up to date        
+    firstBeat = true;                      // set these to avoid noise
+    secondBeat = true;                     // when we get the heartbeat back
+  }
+
+
   sei();                                     // enable interrupts when youre done!
 }// end isr
+
 
 
 
